866d8f2e239c3b0fc9e3dc03e8bbdcafefb12847
flowcore-platform <gitops@flowcore.io>
2024-12-11 23:05:45 +0000

    refactor: :rotating_light: fixed additional linting issues

--- tests/TodoTestSuite.Tests/utility/DockerComposer.cs
+++ tests/TodoTestSuite.Tests/utility/DockerComposer.cs
@@ -2,46 +2,46 @@ namespace TodoTestSuite.Tests.utility;
 
 public class DockerComposer
 {
-  public static void Up(string pathToFile = "./docker-compose.yml")
-  {
-    var process = new System.Diagnostics.Process
+    public static void Up(string pathToFile = "./docker-compose.yml")
     {
-      StartInfo = new System.Diagnostics.ProcessStartInfo
-      {
-        FileName = "docker-compose",
-        Arguments = $"-f {pathToFile} up -d --wait --remove-orphans",
-        RedirectStandardOutput = true,
-        RedirectStandardError = true,
-        UseShellExecute = false
-      }
-    };
+        var process = new System.Diagnostics.Process
+        {
+            StartInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "docker-compose",
+                Arguments = $"-f {pathToFile} up -d --wait --remove-orphans",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            }
+        };
 
-    process.Start();
-    var output = process.StandardOutput.ReadToEnd();
-    var error = process.StandardError.ReadToEnd();
-    process.WaitForExit();
+        process.Start();
+        var output = process.StandardOutput.ReadToEnd();
+        var error = process.StandardError.ReadToEnd();
+        process.WaitForExit();
 
-    if (process.ExitCode != 0)
-    {
-      throw new Exception($"Docker compose failed: {error}");
+        if (process.ExitCode != 0)
+        {
+            throw new Exception($"Docker compose failed: {error}");
+        }
     }
-  }
 
-  public static void Down(string pathToFile = "./docker-compose.yml")
-  {
-    var process = new System.Diagnostics.Process
+    public static void Down(string pathToFile = "./docker-compose.yml")
     {
-      StartInfo = new System.Diagnostics.ProcessStartInfo
-      {
-        FileName = "docker-compose",
-        Arguments = $"-f {pathToFile} down --remove-orphans",
-        RedirectStandardOutput = true,
-        RedirectStandardError = true,
-        UseShellExecute = false
-      }
-    };
+        var process = new System.Diagnostics.Process
+        {
+            StartInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "docker-compose",
+                Arguments = $"-f {pathToFile} down --remove-orphans",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            }
+        };
 
-    process.Start();
-    process.WaitForExit();
-  }
+        process.Start();
+        process.WaitForExit();
+    }
 }
